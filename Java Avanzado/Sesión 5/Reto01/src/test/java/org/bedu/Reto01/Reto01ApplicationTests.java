f281dbfa60d05fa5d72510941cd3081423a839b4
-Harumi- <162361129+Esme-Lopez@users.noreply.github.com>
2025-06-19 03:36:13 -0600

    Retos 1 y 2 sesión 5 terminados

---
+++ Java Avanzado/Sesión 5/Reto01/src/test/java/org/bedu/Reto01/Reto01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.Reto01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
